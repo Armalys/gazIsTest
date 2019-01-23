9567be4854f21ea18b07cc975c094547090bdd98
Armalys <mashnert@mail.ru>
2019-01-23 20:15:46 +0300

    add Mock Tests

--- src/ru/gazis/exception/NotExistStorageException.java
+++ src/ru/gazis/exception/NotExistStorageException.java
@@ -2,6 +2,10 @@ package ru.gazis.exception;
 
 public class NotExistStorageException extends StorageException {
 
+    public NotExistStorageException() {
+        this(null);
+    }
+
     public NotExistStorageException(String name) {
         super("User " + name + " not exist", name);
     }
